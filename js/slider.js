a4ae2609efb347158458c1d0e657b912b28e6f0e
thevostle <technic.dima@gmail.com>
2021-10-07 00:46:00 +0300

    Адаптив - fix 3

--- js/slider.js
+++ js/slider.js
@@ -2,11 +2,14 @@ const btn_prev = document.querySelector(".slider-prev");
 const btn_next = document.querySelector(".slider-next");
 
 const slides = document.getElementsByClassName("slide__data");
+const sliderPoints = document.getElementsByClassName("slider-point");
 
 let idx = 0;
 
 function changeSlide (event) {
     slides[idx].style.opacity = 0;
+    slides[idx].classList.remove("slide_active");
+    sliderPoints[idx].classList.remove("slider-point_active");
 
     let isForward = event.currentTarget.classList[2] == "slider-next";
     if (isForward) idx++;
@@ -15,12 +18,8 @@ function changeSlide (event) {
     if (idx == 5) idx = 0;
     if (idx == -1) idx = 4;
 
-    for (let slide of slides)
-        slide.classList.remove('slide_active');
-        //slide.style.display = "none";
-
-    //slides[idx].style.display = "block";
     slides[idx].classList.add("slide_active");
+    sliderPoints[idx].classList.add("slider-point_active");
     setTimeout(() => {
         slides[idx].style.opacity = 1;
     }, 100);
